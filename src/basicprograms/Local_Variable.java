155328040e8ddf7920d60c66a7cd2e505139bcff
G-arima <garima.lekhi1994@outlook.com>
2024-07-11 07:07:17 +0530

    Second Assignment Submission

---
+++ src/basicprograms/Local_Variable.java
@@ -0,0 +1,17 @@
+package basicprograms;
+
+public class Local_Variable {
+	
+	static void add()
+	{
+	int age;
+	age=90;
+	System.out.println(age);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      add();
+	}
+
+}

988c63e0c44c71689d700cdd19af44f20a4d3118
G-arima <garima.lekhi1994@outlook.com>
2024-07-23 06:51:28 +0530

    Assignment 30 , 31, 32

---
+++ src/basicprograms/String_Functions.java
@@ -0,0 +1,15 @@
+package basicprograms;
+
+public class String_Functions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="MANISH KUMAR TIWARI";
+		System.out.println(s.toLowerCase());
+		System.out.println(s.substring(0, 6));
+		System.out.println(s.substring(7, 12));
+		System.out.println(s.substring(13, 19));
+
+	}
+
+}

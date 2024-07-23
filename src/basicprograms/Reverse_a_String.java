552e7ae5ee8af027cc7b05e166d3af81448246bd
G-arima <garima.lekhi1994@outlook.com>
2024-07-23 23:04:57 +0530

    Assignment 33

---
+++ src/basicprograms/Reverse_a_String.java
@@ -0,0 +1,16 @@
+package basicprograms;
+
+public class Reverse_a_String {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+        String s="Anurag";
+        for(int i=s.length()-1;i>=0;i--)
+        {
+        	System.out.println(s.charAt(i));
+        }
+	}
+
+}
